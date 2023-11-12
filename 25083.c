f8eb862c7ca216710f511995c22a236fbc038b8d
SeongMin1000 <83529503+SeongMin1000@users.noreply.github.com>
2023-11-13 02:06:09 +0900

    Add files via upload

---
+++ 25083.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    printf("         ,r'\"%d\n",7);
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |");
+    return 0;
+}
