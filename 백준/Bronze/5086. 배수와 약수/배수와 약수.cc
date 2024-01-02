9d177804b5ea82642a394775a60699e41dbdfc17
SeongMin1000 <83529503+SeongMin1000@users.noreply.github.com>
2024-01-02 20:35:23 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n ,m;
+    while(1){
+        cin>>n>>m;
+        if(n==0 && m==0) break;
+
+        if(m%n==0) cout<<"factor\n";
+        else if(n%m==0) cout<<"multiple\n";
+        else cout<<"neither\n";
+
+    }
+}
