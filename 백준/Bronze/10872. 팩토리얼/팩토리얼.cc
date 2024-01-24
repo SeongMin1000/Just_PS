d2aa9e6b251762f5f3ea3d1cf3918d574f8ac209
SeongMin1000 <83529503+SeongMin1000@users.noreply.github.com>
2024-01-24 20:22:06 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int a){
+    if(a==0 || a==1) return 1;
+    else return factorial(a-1)*a;
+}
+int main() {
+    cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+    cout<<factorial(n);
+    return 0;
+}
