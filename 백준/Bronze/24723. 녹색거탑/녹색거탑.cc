24ba0463f79fb91e58f6882adfc7d4604edbcf48
SeongMin1000 <83529503+SeongMin1000@users.noreply.github.com>
2024-01-24 20:20:16 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+    int n;
+    cin>>n;
+    cout<<pow(2,n);
+    return 0;
+}
