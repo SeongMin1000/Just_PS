c1176761a80f21433a9edea2eb1738d91d1e5b69
SeongMin1000 <83529503+SeongMin1000@users.noreply.github.com>
2024-02-06 10:55:29 +0900

    [Silver II] Title: N과 M (11), Time: 120 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[8];
+int v[8];
+int n,m;
+void recur(int dep){
+    if(dep==m){
+        for(int i=0; i<m; i++) cout<<v[i]<<" ";
+        cout<<"\n";
+        return;
+    }
+    int pre=0;
+    for(int i=0; i<n; i++){
+        if(pre==arr[i]) continue;
+        pre=arr[i];
+        v[dep]=arr[i];
+        recur(dep+1);
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+	cin.tie(0); //cout.tie(0);
+   
+    cin>>n>>m;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    recur(0);
+    return 0;
+}
