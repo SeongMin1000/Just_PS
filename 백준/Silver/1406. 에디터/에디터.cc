24caf6a176f5cae7e586d6851e43993330338eb1
nature_min <83529503+SeongMin1000@users.noreply.github.com>
2026-03-09 16:22:24 +0900

    [Silver II] Title: 에디터, Time: 84 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,41 +1,59 @@
-#include <iostream>
-#include <list>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
+int main()
+{
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	ios::sync_with_stdio(false);
 
     list<char> l;
+
     string s;
+    cin >> s;
+    for (auto a : s)
+    {
+        l.push_back(a);
+    }
+
+    auto cur = l.end();
+
     int n;
-    cin>>s>>n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string a;
+        cin >> a;
+        if (a == "L")
+        {
+            if (cur != l.begin())
+                cur--;
+        }
+        else if (a == "D")
+        {
+            if (cur != l.end())
+                cur++;
+        }
+        else if (a == "B")
+        {
+            if (cur != l.begin())
+            {
+                cur--;
+                cur = l.erase(cur);
+            }
+        }
+        else
+        {
+            char add;
+            cin >> add;
+            l.insert(cur, add);
+        }
+    }
 
-    for(int i=0; i<s.size(); i++){
-        l.push_back(s[i]);
-    }
-    list<char>::iterator cur=l.end();
-    
-    while(n--){
-        char c;
-        cin>>c;
-        if(c=='L'){
-            if(cur!=l.begin()) cur--;
-        }
-        else if(c=='D'){
-            if(cur!=l.end()) cur++;
-        }
-        else if(c=='B'){
-            if(cur!=l.begin()) cur=l.erase(--cur);
-        }
-        else if(c=='P'){
-            char a;
-            cin>>a;
-            l.insert(cur,a);
-        }
+    for (auto a : l)
+    {
+        cout << a;
     }
 
-    for(auto c:l){
-        cout<<c;
-    }
+    return 0;
 }
