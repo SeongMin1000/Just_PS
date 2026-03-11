e9ecc62cbc0e06779c3589cd38f5dd6f80d05c23
nature_min <83529503+SeongMin1000@users.noreply.github.com>
2026-03-11 17:00:00 +0900

    [Gold IV] Title: 오큰수, Time: 188 ms, Memory: 12504 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,35 +1,39 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-stack<pair<int,int>> nge;
-
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    
+    cin.tie(NULL);
+
     int n;
-    cin>>n;
-    vector<int> arr(n, -1);
-    
-    for(int i=0; i<n; i++){
-        int a;
-        cin>>a;
-        
-        while(!nge.empty()){
-            if(nge.top().second>=a){
-                nge.push({i,a});
-                break;
-            }
-            arr[nge.top().first]=a;
-            nge.pop();
-        }
-        if(nge.empty()){
-            nge.push({i,a});
-        }
+    cin >> n;
+
+    vector<int> arr(n, 0);
+    vector<int> a(n, -1);
+    stack<int> s;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
 
-    for(auto a: arr){
-        cout<<a<<" ";
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!s.empty() && s.top() <= arr[i])
+        {
+            s.pop();
+        }
+
+        if (s.empty())
+            a[i] = -1;
+        else
+            a[i] = s.top();
+        s.push(arr[i]);
     }
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << ' ';
     return 0;
 }
