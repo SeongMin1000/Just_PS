3653983b1356382231d3eb1f7c9d1be331cb0248
nature_min <83529503+SeongMin1000@users.noreply.github.com>
2026-03-18 17:39:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,31 +1,39 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int arr[100002];
 int dis[100002];
 
-int main() {
+int main()
+{
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	ios::sync_with_stdio(false);
 
-    int m,n;
-    cin>>n>>m;
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < 100002; i++)
+    {
+        dis[i] = -1;
+    }
+    dis[n] = 0;
+
     queue<int> q;
-    fill(dis,dis+100001,-1);
-    dis[n]=0;
     q.push(n);
-
-    while(dis[m]==-1){
-        int cur=q.front();
+    while (dis[k] == -1)
+    {
+        int cur = q.front();
         q.pop();
-        for(int nxt: {cur-1,cur+1,2*cur}){
-            if(nxt<0 || nxt>100000) continue;
-            if(dis[nxt]!=-1) continue;
-            dis[nxt]=dis[cur]+1;
-            q.push(nxt);
+        for (auto a : {cur - 1, cur + 1, 2 * cur})
+        {
+            if (a < 0 || a > 100000)
+                continue;
+            if (dis[a] != -1)
+                continue;
+            dis[a] = dis[cur] + 1;
+            q.push(a);
         }
     }
-    cout<<dis[m];
+    cout << dis[k];
     return 0;
 }
