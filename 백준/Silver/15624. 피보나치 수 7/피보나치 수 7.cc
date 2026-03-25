81d12b983eefdcfe787f804038cb77e4969cb690
nature_min <83529503+SeongMin1000@users.noreply.github.com>
2026-03-25 23:44:33 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 4 ms, Memory: 5928 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[1000002];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 1000000007;
+    }
+
+    cout << arr[n];
+    return 0;
+}
