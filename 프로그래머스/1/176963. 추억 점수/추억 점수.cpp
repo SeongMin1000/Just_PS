bb038e1519d0f81f03fff5fffabc5697f4a7a8ba
nature_min <83529503+SeongMin1000@users.noreply.github.com>
2026-05-08 22:00:18 +0900

    [level 1] Title: 추억 점수, Time: 1.06 ms, Memory: 4.33 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    
+    map<string,int> m;
+    for(int i=0; i<name.size(); i++){
+        m[name[i]]=yearning[i];
+    }
+    
+    for(int i=0; i<photo.size(); i++){
+        int tmp=0;
+        for(auto a: photo[i]){
+            tmp+=m[a];
+        }
+        answer.push_back(tmp);
+    }
+    
+    return answer;
+}
