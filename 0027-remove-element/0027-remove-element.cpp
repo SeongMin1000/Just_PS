c87118bf336a51c8aaa6b090a08e51e16f9ef06e
nature_min <83529503+SeongMin1000@users.noreply.github.com>
2026-07-09 10:36:23 +0900

    Time: 0 ms (100%), Space: 11.9 MB (12.28%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int w=0;
+        for(int i=0; i<nums.size(); i++){
+            if(val!=nums[i]){
+                nums[w]=nums[i];
+                w++;
+            }
+        }
+        return w;
+    }
+};
