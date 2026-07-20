ec85a3a6905165b80bf2083937244e8e02d0f50a
nature_min <83529503+SeongMin1000@users.noreply.github.com>
2026-07-20 11:31:33 +0900

    Time: 0 ms (100%), Space: 97.4 MB (25.47%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0;
+        int minPrice=prices[0];
+
+        for(int i=1; i<prices.size(); i++){
+            minPrice=min(minPrice, prices[i]);
+            maxProfit=max(prices[i]-minPrice, maxProfit);
+        }
+        
+        return maxProfit;
+    }
+};
